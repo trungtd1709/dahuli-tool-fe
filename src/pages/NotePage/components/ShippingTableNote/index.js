364e1d3d4de92be3a8d200fd468ef559c3e05373
trungta17 <trung.ta@lge.com>
2025-02-23 01:37:18 +0700

    update shipping

--- src/pages/NotePage/components/ShippingTableNote/index.js
+++ src/pages/NotePage/components/ShippingTableNote/index.js
@@ -14,9 +14,11 @@ export const ShippingTableNote = () => {
   const data = [["Sample Product", 10, 10, 50, 500, 75]];
 
   return (
-    <>
+    <div>
       <span>File shipping note</span>
+      <span>Shipping mặc định là international. Nếu là domestic thì thêm chữ domestic trong tên</span>
+      <span>VD: Domestic Shipping from eBay to Bestone - S304</span>
       <AppTable headers={headers} data={data} />
-    </>
+    </div>
   );
 };
