d085b72b18adbd8385b685cbcdac056732fa236f
trungta17 <trung.ta@lge.com>
2025-04-28 00:19:23 +0700

    update

--- src/pages/NotePage/components/ShippingTableNote/index.js
+++ src/pages/NotePage/components/ShippingTableNote/index.js
@@ -17,6 +17,7 @@ export const ShippingTableNote = () => {
     <div style={{display: 'flex', flexDirection: 'column'}}>
       <span>File shipping note</span>
       <span>Shipping mặc định là international. Nếu là domestic thì thêm chữ domestic trong tên</span>
+      <span>Trong trường hợp chỉ có một số SKU mới mất phí ship thì điền tên các SKU vào trong tên của chuyến ship</span>
       <span>VD: Domestic Shipping from eBay to Bestone - S304</span>
       <AppTable headers={headers} data={data} />
     </div>
